0af5c3a74c0adeed236be2f7360078c06bfd8afd
joyceandronicco <joyceandronico@msn.com>
2022-10-21 12:23:38 -0300

    feat: :sparkles: desafio 14 - Crie uma query que retorne todos os lanches que possuem picles  em seus ingredientes e mostre apenas os 3 primeiros itens contidos  no array valoresNutricionais

---
+++ challenges/desafio14.js
@@ -0,0 +1,8 @@
+/* Crie uma query que retorne todos os lanches que possuem picles
+ em seus ingredientes e mostre apenas os 3 primeiros itens contidos
+  no array valoresNutricionais */
+
+db.getCollection("produtos").find(
+    { ingredientes: { $all: ["picles"] } },
+    { _id: 0, nome: 1, ingredientes: 1, valoresNutricionais: { $slice: 3 } },
+);
