5fef136b75967042229385003bc5fd490391aa29
joyceandronicco <joyceandronico@msn.com>
2022-10-25 09:18:28 -0300

    feat: :sparkles: Adiciona o campo avaliacao em todos os documentos da coleção  e efetua alterações nesse campo

---
+++ challenges/desafio15.js
@@ -0,0 +1,28 @@
+/* Adicione o campo avaliacao em todos os documentos da coleção
+ e efetue alterações nesse campo */
+
+// Crie uma query que inclua o campo avaliacao do tipo NumberInt, com o valor 0 em todos os documentos da coleção.
+
+db.getCollection("produtos").updateMany(
+    {}, { $set: { avaliacao: 0 } },
+);
+
+// Crie uma query que incremente o valor do campo avaliacao em 5 em todos os sanduíches de carne do tipo bovino. Utilize como filtro o campo tags.
+
+db.getCollection("produtos").updateMany(
+    { tags: { $elemMatch: { $eq: "bovino" } } },
+    { $inc: { avaliacao: 5 } },
+);
+
+// Crie uma query que incremente o valor do campo avaliacao em 3 em todos os sanduíches de ave.
+
+db.getCollection("produtos").updateMany(
+    { tags: { $elemMatch: { $eq: "ave" } } },
+    { $inc: { avaliacao: 3 } },
+);
+
+// Crie uma query que retorne o nome e avaliacao de todos os sanduíches.
+
+db.getCollection("produtos").find(
+    {}, { _id: 0, nome: 1, avaliacao: 1 },
+);
