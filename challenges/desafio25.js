86f84a3073e6ce4a7c0145675ac7f6c4c736120f
joyceandronicco <joyceandronico@msn.com>
2022-10-25 11:09:05 -0300

    feat: :sparkles: Adiciona o valor muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40

---
+++ challenges/desafio25.js
@@ -0,0 +1,17 @@
+/* Adicione o valor muito sódio ao final do array tags nos produtos em que o
+percentual de sódio seja maior ou igual a 40 */
+
+/* Crie uma query que faça a adição do valor muito sódio ao final do array tags nos produtos em
+que o percentual de sódio seja maior ou igual a 40. */
+
+db.getCollection("produtos").updateMany({
+    valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } },
+},
+    { $push: { tags: { $each: ["muito sódio"] } } });
+
+// Crie uma query que retorne o nome e tags de todos os documentos.
+
+db.getCollection("produtos").find(
+    {},
+    { _id: 0, nome: 1, tags: 1 },
+);
